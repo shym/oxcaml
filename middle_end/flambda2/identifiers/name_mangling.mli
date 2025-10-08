962965a06e49d0a34a345cc6291711649239c368
Tim McGilchrist <timmcgil@gmail.com>
2025-10-08 14:14:10 +0800

    Add name mangling interface.

---
+++ middle_end/flambda2/identifiers/name_mangling.mli
@@ -0,0 +1,17 @@
+type path_item =
+  | Module of string
+  | AnonymousFunction of int * int * string option
+  | NamedFunction of string
+  | AnonymousModule of int * int * string option
+
+(*
+  let n = [Module "Foo"; Module "Bar"; NamedFunction "baz"]
+ *)
+type path = path_item list
+
+(** Transform a [path] into a mangled name suitable for creating a LinkageName.t *)
+val mangle_path : path -> string
+
+val mangle_comp_unit : Compilation_unit.t -> string
+
+val mangle_ident : Compilation_unit.t -> path -> string
