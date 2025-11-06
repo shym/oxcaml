9e737547db291ae300c480c36aa41571535584aa
Samuel Hym <samuel@tarides.com>
2025-11-06 15:39:48 +0100

    Refine the stamp-hiding script
    On lines with multiple identifiers appearing, the `(.*)` can match, not
    only the body of one identifier but everything from the start of the
    body of the first identifier to the end of the body of the last
    identifier of a line

--- oxcaml/tests/backend/llvmize/filter.sh
+++ oxcaml/tests/backend/llvmize/filter.sh
@@ -4,8 +4,9 @@
 sed "s/[ \t]*\n/\n"/ | \
 sed "s/[ \t]*$//" | \
 
-# hide flaky parts of identifiers
-sed -r 's/_O(.*)_[0-9]+(_code)?/_O\1_HIDE_STAMP/' | \
+# hide flaky parts of identifiers, both in .data and .text sections
+sed -r 's/caml([0-9A-Za-z_]*)_[0-9]+/caml\1_HIDE_STAMP/g' | \
+sed -r 's/_O([0-9A-Za-z_]*)_[0-9]+_code/_O\1_HIDE_STAMP/g' | \
 # sed 's/__/./' | \
 
 # hide target triple
